af2f69a4cc856cd904dbdd34c1819ff7963d9685
Vimal Trivedi <vtrivedi@educatingkids.com.au>
2021-07-27 16:37:13 +1000

    removed unused package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const xlsx = require('xlsx');
 const htmltoText = require('html-to-formatted-text');
 
+
 const wb = xlsx.readFile("Book1.xlsx");
 const ws = wb.Sheets["Sheet1"];
 const data = xlsx.utils.sheet_to_json(ws);
@@ -12,8 +13,9 @@ const newData = data.map((row)=>{
     return row;
 })
 
+const newFileName= "New File.xlsx";
 const newWb= xlsx.utils.book_new();
 const newWs = xlsx.utils.json_to_sheet(newData);
 xlsx.utils.book_append_sheet(newWb,newWs,"New Data");
-xlsx.writeFile(newWb,"New file.xlsx");
-console.log("Operation completed!!");
+xlsx.writeFile(newWb,newFileName);
+console.log(`A new file created with Name ${newFileName}`);
